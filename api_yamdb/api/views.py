836e7c91a96e8bb7d646394d182f7088fa48391e
Gorstka <gorstkasergei@gmail.com>
2022-01-30 16:01:01 +0300

    3

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -128,6 +128,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrAdminOrModerator,)
     pagination_class = CommentsPagination
 
+    def get_queryset(self):
+        review_id = self.kwargs.get("review_id")
+        review = get_object_or_404(Review, pk=review_id)
+        comments = review.comments.all()
+        return comments
+
     def perform_create(self, serializer):
         review_id = self.kwargs.get("review_id")
         review = get_object_or_404(Review, pk=review_id)
@@ -141,6 +147,12 @@ class ReviewViewSet(viewsets.ModelViewSet):
         permissions.IsAuthenticatedOrReadOnly,
         IsAuthorOrAdminOrModerator,
     )
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, pk=title_id)
+        reviews = title.reviews.all()
+        return reviews
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
